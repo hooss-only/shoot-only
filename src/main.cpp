04b295a852d5df5991c08e283304bfb2da98224b
hooss-only <hoossonly@gmail.com>
2025-09-23 12:15:33 +0900

    Add simple black screen

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(int argc, char* argv[]) {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                return 0;
+        }
+        
+        SDL_Window *window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (!window) {
+                return 0;
+        } else {
+                SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+                if (!renderer) {
+                        return 0;
+                } else {
+                        bool running = true;
+                        while (running) {
+                                SDL_Event e;
+
+                                SDL_WaitEvent(&e);
+                                
+                                if (e.type == SDL_QUIT) {
+                                        running = false;
+                                }
+                                
+                                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+                                SDL_RenderClear(renderer);
+                                
+                                SDL_RenderPresent(renderer);
+                        }
+
+                        SDL_DestroyRenderer(renderer);
+                }
+                SDL_DestroyWindow(window);
+        }
+        SDL_Quit();
+
+        return 0;
+}
