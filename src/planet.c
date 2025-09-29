1cccdd12db7399bb2dbbffc5d5ebca317c3c706d
hooss-only <hoossonly@gmail.com>
2025-09-29 19:27:07 +0900

    Add planets breaks missiles

--- src/planet.c
+++ src/planet.c
@@ -1,8 +1,7 @@
 #include "planet.h"
 
 #include "textures.h"
-
-#include "utils/logger.h"
+#include "missiles.h"
 
 void init_planet(struct planet* self, SDL_Renderer* renderer, struct player* player) {
         self->existance = true;
@@ -37,6 +36,13 @@ void tick_planet(struct planet* self, float dt) {
         int r = self->rect.w / 2;
         if (check_collision(self->x + r, self->y + r, self->player->x + 8, self->player->y + 8, r)) {
                 self->player->vy = self->speed + 50;
+        }
+        
+        for (int i=0; i<MAX_MISSILE; i++) {
+                struct missile* m = &missiles[i];
+                if (check_collision(self->x + r, self->y + r, m->x + m->rect.w/2.f, m->y + m->rect.w/2.f, r)) {
+                        m->existance = false;
+                }
         }
 }
 
